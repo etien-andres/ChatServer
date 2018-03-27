c5b30731e929eac1c5a6eb75408ac5ac54320d32
etien-andres <andres.etien@gmail.com>
2018-03-26 18:17:40 -0600

    good
    test

---
+++ chatencr/src/chatencr/Chatencr.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatencr;
+
+/**
+ *
+ * @author Hck
+ */
+public class Chatencr {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+}
